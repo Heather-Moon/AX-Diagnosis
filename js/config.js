87fd94d0ba47423c50924e5b65cc92aa9b4c0bf5
hyewon-m <hyewon.moon@codepresso.kr>
2026-06-09 16:49:34 +0900

    변경 사항 적용

---
+++ js/config.js
@@ -0,0 +1,2 @@
+// Google Apps Script 웹 앱 URL — 배포 후 여기에 입력
+const SHEETS_API_URL = 'https://script.google.com/a/macros/codepresso.kr/s/AKfycby2gDwH45dZqACGEtVR7UcpbiSGVya901TjiukIvc--41jgtt2RERJEJeaEfX5reqopdA/exec';
